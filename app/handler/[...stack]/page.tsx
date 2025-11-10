491dcc9aaace8c5b1798dd6fd893e7e32b52704e
daikod <daikod@outlook.com>
2025-11-10 14:32:19 +0100

    updated stack handler

--- app/handler/[...stack]/page.tsx
+++ app/handler/[...stack]/page.tsx
@@ -1,7 +1,7 @@
 import { StackHandler } from "@stackframe/stack"; 
 import { stackServerApp } from "../../../stack/server"; 
 
-export default function Handler(props: unknown) { 
+export default function Handler(props: { params: unknown, searchParams: unknown }) { 
    return ( <StackHandler
       app={stackServerApp}
       routeProps={props}
